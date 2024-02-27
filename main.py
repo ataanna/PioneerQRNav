61a32f68a8c24e871a4d81a950793f99d37fdf93
ataa <atanufayl@gmail.com>
2024-02-27 11:58:47 +0700

    qrscanner competition code

--- main.py
+++ main.py
@@ -2,4 +2,31 @@ import pioneer
 import qrscanner
 
 pn = pioneer.Pioneer()
-qrscanner.open_window()
+# pn.move_pioneer(0.9,0.9)
+
+while True:
+    qrscanner.open_window()
+    val = qrscanner.qr_detect()
+    print(val)
+
+    if val == "maju": 
+        pn.move_pioneer(3,3)
+
+    elif val == "mundur":
+        pn.move_pioneer(-1,-1)
+
+    elif val == "kanan":
+        pn.move_pioneer(2,-2)
+
+    elif val == "kiri":
+        pn.move_pioneer(-2,2)
+
+
+    else:
+        pn.move_pioneer(0,0)
+
+
+        
+   
+
+
